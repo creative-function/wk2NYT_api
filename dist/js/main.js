2527e7286df82a69e104ade0fb3b5c0c164b9c21
Chelsea <chelsea.campbell@my.creativecirucs.edu>
2019-07-23 12:47:34 -0400

    updated
    works but doesn't display results

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,11 +7,11 @@
 (function () {
   // ;( <-- starting it with ; seperates it from other code
   console.log('test2');
-  var API_URL_BASE = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=[key]&api-key=[aCzqpe8z4mtAFyQ1e1fJbxXrJUJuq8zD';
-  var searchEl = document.querySelector('[name="key"]'); //const valueEl = document.querySelector('[name="value"]')
-
+  var API_URL_BASE = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+  var searchEl = document.querySelector('[name="key"]');
+  var resultBox = document.querySelector('.resultBox');
   var getBtn = document.querySelector('[name="get"]');
-  var postBtn = document.querySelector('[name="post"]');
+  var myKey = "aCzqpe8z4mtAFyQ1e1fJbxXrJUJuq8zD";
   var statusLbl = document.querySelector('[name="status"]'); /////////////GET REQUEST
 
   function makeGetRequest(event) {
@@ -28,23 +28,25 @@
 
   function handleGetResponse(response) {
     //updates and displays new values in Value input
-    console.log('response!', response); //the tree to get to the value is inside response, inside data, inside another data, and there is the value. i.e  
-    // response:
-    //     data[
-    //         data{
-    //            value: "hello"
-    //            id: "example"
-    //         }
-    //     ]
-    // const value = response.data.data.value
-    // //upatdae valueEl's value to = value variable 
-    // valueEl.value = value
+    console.log('response!', response);
+    var articles = data.response.docs;
+
+    for (var i = 0; i < articles.length; i++) {
+      var searchResult = document.createElement("div"); //give it content
+
+      var searchData = articles[i].headline.main;
+      searchResult.appendChild(searchData);
+      document.body.appendChild(searchResult);
+      console.log("articles added");
+    }
   }
 
   function getData(key) {
-    console.log('fetching with GET:' + key); //axios go to this url, fetch this value, then call this function 
+    console.log('fetching with GET:' + key);
+    console.log('fetching:' + API_URL_BASE); //axios go to this url, fetch this value, then call this function 
 
-    axios.get(API_URL_BASE).then(handleGetResponse);
+    axios.get(API_URL_BASE + "?q=" + key + '&api-key=' + myKey).then(handleGetResponse);
+    console.log(API_URL_BASE);
   }
 })();
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,13 @@
 
 ;(function(){ // ;( <-- starting it with ; seperates it from other code
     console.log ('test2')
-    const API_URL_BASE= 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=[key]&api-key=[aCzqpe8z4mtAFyQ1e1fJbxXrJUJuq8zD'
+    const API_URL_BASE= 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
     const searchEl = document.querySelector('[name="key"]')
-    //const valueEl = document.querySelector('[name="value"]')
+    const resultBox = document.querySelector('.resultBox')
     const getBtn = document.querySelector('[name="get"]')
-    const postBtn = document.querySelector('[name="post"]')
+    const myKey = "aCzqpe8z4mtAFyQ1e1fJbxXrJUJuq8zD";
     const statusLbl = document.querySelector('[name="status"]')
+    
 
     /////////////GET REQUEST
 
@@ -20,29 +21,33 @@
         console.log('value of key: ', searchValue)
         getData(searchValue)
     }
-
         
     //when user clicks button, run this function 
     getBtn.addEventListener('click', makeGetRequest)
 
     function handleGetResponse(response){ //updates and displays new values in Value input
         console.log('response!', response)
-                    //the tree to get to the value is inside response, inside data, inside another data, and there is the value. i.e  
-                    // response:
-                    //     data[
-                    //         data{
-                    //            value: "hello"
-                    //            id: "example"
-                    //         }
-                    //     ]
-        // const value = response.data.data.value
-        // //upatdae valueEl's value to = value variable 
-        // valueEl.value = value
+        
+        var articles =  data.response.docs;
+
+        
+        for (var i= 0; i < articles.length; i++){
+
+            let searchResult = document.createElement("div");
+            //give it content
+            let searchData = articles[i].headline.main;
+        
+            searchResult.appendChild(searchData);
+            document.body.appendChild(searchResult);
+            console.log("articles added")
+        }
     }
 
     function getData(key){ 
         console.log('fetching with GET:' + key)
+        console.log('fetching:' + API_URL_BASE)
         //axios go to this url, fetch this value, then call this function 
-        axios.get(API_URL_BASE).then(handleGetResponse)
+        axios.get(API_URL_BASE + "?q=" + key + '&api-key=' + myKey).then(handleGetResponse)
+        console.log(API_URL_BASE)
     }
 })()
